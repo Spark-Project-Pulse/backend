0a97afebe2401cb74fc4e0cde6a5e421cfc35fc8
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-09-29 17:18:44 -0400

    added frontend api to cors allowed origins

--- backend/development.py
+++ backend/development.py
@@ -1,5 +1,7 @@
 DEBUG=True
+
 ALLOWED_HOSTS = ["*"]
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
--- backend/production.py
+++ backend/production.py
@@ -1,2 +1,9 @@
+import os
+
 DEBUG=False
+
 ALLOWED_HOSTS = ['pulse-backend-704608178414.us-east4.run.app']
+
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get('CORS_ALLOWED_ORIGIN'),
+]
