68a042dbbc655f0bfcf02f11bd7c1335d29a4db4
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-09-30 22:15:38 -0400

    Require method to be post (might not be necessary cause supabase is smart)

--- backend/views/question_views.py
+++ backend/views/question_views.py
@@ -4,12 +4,15 @@ from ..repositories import question_repository
 
 # A function that calls the repository layer to create a question and returns a JsonResponse based on the result
 def createQuestion(request):
-    try:
-        question_data = request.POST.dict()
-        response = question_repository.create_question(question_data)
-        return JsonResponse({"status": "success", "data": response.data})
-    except Exception as e:
-        return JsonResponse({"status": "error", "message": str(e)}, status=500)
+    if request.method == 'POST':
+        try:
+            question_data = request.POST.dict()
+            response = question_repository.create_question(question_data)
+            return JsonResponse({"status": "success", "data": response.data})
+        except Exception as e:
+            return JsonResponse({"status": "error", "message": str(e)}, status=500)
+    else:
+        return JsonResponse({"status": "error", "message": "Only POST requests are allowed."}, status=400)
         
 # A function that calls the repository layer to get all questions and returns a JsonResponse based on the result
 def getAllQuestions(request):
