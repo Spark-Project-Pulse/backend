580ae2c965851402300f0498b3bbedab604ac169
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-10-04 12:57:06 -0400

    fetching projects working too

--- pulse/urls/project_urls.py
+++ pulse/urls/project_urls.py
@@ -5,6 +5,6 @@ from ..views import project_views
 # URL routes for calls relating to projects
 urlpatterns = [
     path('create/', project_views.createProject, name='createProject'),
-    # path('getAll/', project_views.getAllProjects, name='getAllProjects'),
-    # path('getById/<str:Project_id>/', project_views.getProjectById, name='getProjectById'),
+    path('getAll/', project_views.getAllProjects, name='getAllProjects'),
+    path('getById/<str:project_id>/', project_views.getProjectById, name='getProjectById'),
 ]
--- pulse/views/project_views.py
+++ pulse/views/project_views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpRequest
 from rest_framework import status
+from ..models import Projects
 from ..serializers import ProjectSerializer
 
 @api_view(["POST"])
@@ -23,3 +25,39 @@ def createProject(request: HttpRequest) -> JsonResponse:
             {"project_id": project.project_id}, status=status.HTTP_201_CREATED
         )
     return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def getAllProjects(request: HttpRequest) -> JsonResponse:
+    """
+    Retrieve all projects from the database and serialize them
+    to JSON format.
+
+    TODO: eventually this should have the ability to filter only projects for a specific person... or use other filters (e.g. tags)
+
+    Args:
+        request (HttpRequest): The incoming HTTP request.
+
+    Returns:
+        JsonResponse: A response containing serialized data for all Project.
+    """
+    project = Projects.objects.all()
+    serializer = ProjectSerializer(project, many=True)
+    return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
+def getProjectById(request: HttpRequest, project_id: str) -> JsonResponse:
+    """
+    Retrieve a single project by its ID and serialize it to JSON format.
+
+    Args:
+        request (HttpRequest): The incoming HTTP request.
+        project_id (str): The ID of the project to retrieve.
+
+    Returns:
+        JsonResponse: A response containing serialized data for the requested project.
+    """
+    project = get_object_or_404(Projects, project_id=project_id)
+    serializer = ProjectSerializer(project)
+    return JsonResponse(serializer.data, status=status.HTTP_200_OK)
