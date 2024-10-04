c6cd156cba8554d604b4f3e863c46b08d90aa9e0
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-10-04 12:49:08 -0400

    createProject api working properly

--- pulse/urls/project_urls.py
+++ pulse/urls/project_urls.py
@@ -4,7 +4,7 @@ from ..views import project_views
 
 # URL routes for calls relating to projects
 urlpatterns = [
-    # path('create/', project_views.createProject, name='createProject'),
+    path('create/', project_views.createProject, name='createProject'),
     # path('getAll/', project_views.getAllProjects, name='getAllProjects'),
     # path('getById/<str:Project_id>/', project_views.getProjectById, name='getProjectById'),
 ]
--- pulse/views/project_views.py
+++ pulse/views/project_views.py
@@ -1,6 +1,25 @@
 from rest_framework.decorators import api_view
-from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpRequest
 from rest_framework import status
-from ..models import Questions
 from ..serializers import ProjectSerializer
+
+@api_view(["POST"])
+def createProject(request: HttpRequest) -> JsonResponse:
+    """
+    Create a project using the ProjectSerializer to validate
+    and save the incoming data.
+
+    Args:
+        request (HttpRequest): The incoming HTTP request containing the data.
+    
+    Returns:
+        JsonResponse: A response with the created project's ID if successful,
+        or validation errors if the data is invalid.
+    """
+    serializer = ProjectSerializer(data=request.data)
+    if serializer.is_valid():
+        project = serializer.save()
+        return JsonResponse(
+            {"project_id": project.project_id}, status=status.HTTP_201_CREATED
+        )
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
