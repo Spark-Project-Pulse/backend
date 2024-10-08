24bd271b11ceb9406fc82502aabce871b310fac5
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-10-08 13:42:06 -0400

    Created necessary backend calls for users table (cannot test it yet)

--- pulse/serializers.py
+++ pulse/serializers.py
@@ -1,25 +1,27 @@
 from rest_framework import serializers
-from .models import Questions
-from .models import Answers
-from .models import Questions, Projects
+from .models import Answers, Questions, Projects, Users
 
 # NOTE: Each model should have a corresponding serializer to handle validation and
 # conversion of incoming data, as well as serializing outgoing data to be
 # returned in responses.
 
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Questions
-        fields = '__all__'  # or specify the fields you want
-
-
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answers
         fields = '__all__'  # or specify the fields you want
-
         
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projects
         fields = '__all__'
+        
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Questions
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = '__all__'
+        
--- pulse/urls/__init__.py
+++ pulse/urls/__init__.py
@@ -3,13 +3,15 @@
 from django.urls import include, path
 
 # In order to have seperate url files, django requires this file and to import the urls here:
-from .question_urls import urlpatterns as question_urls
 from .answer_urls import urlpatterns as answer_urls
 from .project_urls import urlpatterns as project_urls
+from .question_urls import urlpatterns as question_urls
+from .user_urls import urlpatterns as user_urls
 
 # All URL routes
 urlpatterns = [
-    path('questions/', include(question_urls)),
     path('answers/', include(answer_urls)),
     path('projects/', include(project_urls)),
+    path('questions/', include(question_urls)),
+    path('users/', include(user_urls)),
 ]
