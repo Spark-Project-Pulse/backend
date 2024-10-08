319bbe32d38316292d848e67ef9ce83140445741
Alex Miller <amiller06880@gmail.com>
2024-10-08 16:22:35 -0400

    fixed allowed hosts to not include https

--- backend/production.py
+++ backend/production.py
@@ -1,6 +1,6 @@
 DEBUG=False
 
-ALLOWED_HOSTS = ["https://pulse-backend-704608178414.us-east4.run.app"]
+ALLOWED_HOSTS = ["pulse-backend-704608178414.us-east4.run.app"]
 
 CORS_ALLOWED_ORIGINS = ["https://pulse-frontend-704608178414.us-east4.run.app"]
 
