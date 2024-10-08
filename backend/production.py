d7de72631f3ff2190f281952d70f0b6e9c87fabc
Alex Miller <amiller06880@gmail.com>
2024-10-08 13:28:59 -0400

    fixed typo in origin environment variable

--- backend/production.py
+++ backend/production.py
@@ -5,7 +5,7 @@ DEBUG=False
 ALLOWED_HOSTS = ['pulse-backend-704608178414.us-east4.run.app']
 
 CORS_ALLOWED_ORIGINS = [
-    os.environ.get('CORS_ALLOWED_ORIGIN'),
+    os.environ.get('CORS_ALLOWED_ORIGINS'),
 ]
 
 print(CORS_ALLOWED_ORIGINS)
