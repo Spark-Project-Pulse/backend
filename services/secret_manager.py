3db42a3f5a20a0f8081395670c4cfff56200b858
Alex Miller <amiller06880@gmail.com>
2024-10-13 22:32:12 -0400

    added service acocunt key to .gitignore

---
+++ services/secret_manager.py
@@ -0,0 +1,26 @@
+from google.auth import exceptions as auth_exceptions
+from google.cloud import secretmanager
+import os
+
+def get_secret(secret_name):
+    """Fetch a secret from Google Secret Manager using a service account key file locally or the default service account in Cloud Run."""
+    
+    try:
+        # Access Google Secret Manager
+        client = secretmanager.SecretManagerServiceClient()
+        
+        project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+        if not project_id:
+            raise Exception("GOOGLE_CLOUD_PROJECT environment variable is not set.")
+        
+        # Build the resource name of the secret
+        name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+        
+        # Access the secret version
+        response = client.access_secret_version(name=name)
+        return response.payload.data.decode("UTF-8")
+    
+    except auth_exceptions.DefaultCredentialsError as e:
+        raise Exception("Failed to access Google Secret Manager: ensure your credentials are set correctly.") from e
+    except Exception as e:
+        raise Exception(f"An error occurred while accessing the secret '{secret_name}' from Google Secret Manager: {e}") from e
