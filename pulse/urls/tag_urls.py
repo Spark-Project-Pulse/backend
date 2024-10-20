5e49cc9f9ce6e19396aa37ad1eb8f40b83a71bea
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-10-20 10:18:32 -0400

    fixed typo

--- pulse/urls/tag_urls.py
+++ pulse/urls/tag_urls.py
@@ -3,5 +3,5 @@ from ..views import tag_views
 
 urlpatterns = [
     path('create/', tag_views.createTag, name='createTag'),
-    path('getAll', tag_views.getAllTags, name='getAllTags'),
+    path('getAll/', tag_views.getAllTags, name='getAllTags'),
 ]
