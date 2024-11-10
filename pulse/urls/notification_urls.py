e9ee6e4b47108d398b7ab2a0d3d298edf6441e4b
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-11-10 17:00:19 -0500

    added endpoint for notifications by user

--- pulse/urls/notification_urls.py
+++ pulse/urls/notification_urls.py
@@ -3,4 +3,5 @@ from ..views import notification_views
 
 urlpatterns = [
     path('getAll/', notification_views.getAllNotifications, name='getAllNotifications'),
+    path('getByUserId/<str:user_id>/', notification_views.getNotificationsByUserId, name='getNotificationsByUserId'),
 ]
--- pulse/views/notification_views.py
+++ pulse/views/notification_views.py
@@ -6,6 +6,28 @@ from ..serializers import NotificationSerializer
 
 
 @api_view(["GET"])
+def getNotificationsByUserId(request: HttpRequest, user_id: int) -> JsonResponse:
+    """
+    Retrieve all Notifications associated with a specific user_id
+    from the database and serialize them to JSON format.
+
+    Args:
+        request (HttpRequest): The incoming HTTP request.
+        user_id (int): The ID of the user whose Notifications are to be retrieved.
+
+    Returns:
+        JsonResponse: A response containing serialized data for the user's Notifications.
+    """
+    notifications = Notifications.objects.filter(recipient_id=user_id).order_by(
+        "-created_at"
+    )  # Retrieve Notifications for the specified user
+    serializer = NotificationSerializer(
+        notifications, many=True
+    )  # Serialize the queryset to JSON
+    return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
+
+
+@api_view(["GET"])
 def getAllNotifications(request: HttpRequest) -> JsonResponse:
     """
     Retrieve all notifications from the database and serialize them
