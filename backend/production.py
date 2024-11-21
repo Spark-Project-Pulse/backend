6a9bd76b08de403046c11dac1dd14c90a7a9dc71
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-11-20 23:50:37 -0500

    made the only allowed origins codehive and pulse

--- backend/production.py
+++ backend/production.py
@@ -3,13 +3,11 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "pulse-frontend-704608178414.us-east4.run.app",
     "codehive.buzz",
-    "www.codehive.buzz",
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "https://pulse-frontend-704608178414.us-east4.run.app",
     "https://codehive.buzz",
-    "https://www.codehive.buzz",
 ]
 
 DATABASE_OPTIONS_SSLMODE = "require"
