4ba90a2bb67fe7b06c1b8a01263087dad1d8e926
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-11-21 00:06:35 -0500

    put bandaid on issue (we will need to fix)

--- backend/production.py
+++ backend/production.py
@@ -1,9 +1,14 @@
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "pulse-frontend-704608178414.us-east4.run.app",
-    "codehive.buzz",
-]
+
+ALLOWED_HOSTS = ['*']
+
+# TODO: we really need to properly configure allowed hosts (for some reason the following does not work)
+
+# ALLOWED_HOSTS = [
+#     "pulse-frontend-704608178414.us-east4.run.app",
+#     "codehive.buzz",
+# ]
 
 CORS_ALLOWED_ORIGINS = [
     "https://pulse-frontend-704608178414.us-east4.run.app",
