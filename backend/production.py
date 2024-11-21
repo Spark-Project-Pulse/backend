33a1d751d9c0fa7d7a45f5ee11f2255b60499d52
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-11-20 23:51:12 -0500

    allowed all hosts

--- backend/production.py
+++ backend/production.py
@@ -1,9 +1,6 @@
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "pulse-frontend-704608178414.us-east4.run.app",
-    "codehive.buzz",
-]
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "https://pulse-frontend-704608178414.us-east4.run.app",
